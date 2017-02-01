83a879275f73befdbdc3dce5fa02ba8d28f1d9e4
tuan6956 <ng.anhtuan20021997@gmail.com>
2017-02-01 20:31:18 +0700

    Add project files.

---
+++ Demo/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 28);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(436, 99);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Location = new System.Drawing.Point(12, 145);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(39, 13);
+            this.output.TabIndex = 4;
+            this.output.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 164);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(436, 99);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(193, 296);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(100, 20);
+            this.txtFilter.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(373, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(373, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Demo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 322);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Demo/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet.Net;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string[] arr = txtInput.Lines.Where(x => x.Contains(txtFilter.Text)).ToArray();
+            txtOutput.Lines = arr;
+            //foreach (var item in txtInput.Lines)
+            //{
+            //    if (item.Contains(txtFilter.Text))
+            //    {
+            //        txtOutput.Text += item + Environment.NewLine;
+
+            //    }
+            //}
+            //foreach (var item in arr)
+            //{
+            //    string[] temp = item.Split('|');
+            //    if (temp[3] == txtFilter.Text)
+            //    {
+            //        txtOutput.Text += item + Environment.NewLine;
+            //    }
+            //}
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //txtInput.Text = File.ReadAllText("ssh.txt");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                txtInput.Text = File.ReadAllText(open.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(save.FileName, txtOutput.Text);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Login
+            try
+            {
+                HttpRequest rq = new HttpRequest();
+                rq.Cookies = new CookieDictionary();
+                //rq.Proxy = new HttpProxyClient("127.0.0.1", 8888);
+                string html = rq.Get("https://id.zing.vn/").ToString();
+                rq.AddParam("pid", "38");
+                rq.AddParam("u1", "https://id.zing.vn/v2/login/cb?apikey=92140c0e46c54994812403f564787c14&pid=38&_src=&utm_source=&utm_medium=&utm_term=&utm_content=&utm_campaign=&next=https%3A%2F%2Fid.zing.vn%2Fv2%2Finfosetting%3Fapikey%3D92140c0e46c54994812403f564787c14%26pid%3D38&referer=");
+                rq.AddParam("fp", "https://id.zing.vn/v2/login/cb?apikey=92140c0e46c54994812403f564787c14&pid=38&_src=&utm_source=&utm_medium=&utm_term=&utm_content=&utm_campaign=&next=https%3A%2F%2Fid.zing.vn%2Fv2%2Finfosetting%3Fapikey%3D92140c0e46c54994812403f564787c14%26pid%3D38&referer=");
+                rq.AddParam("apikey", "92140c0e46c54994812403f564787c14");
+                rq.AddParam("u", "thanhps422");
+                rq.AddParam("p", "123123123");
+                rq.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
+                rq.Referer = "https://id.zing.vn/v2/login?apikey=92140c0e46c54994812403f564787c14&data=V8qMSFbMj09UY_q9eg";
+                html = rq.Post("https://sso3.zing.vn/alogin").ToString();
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+                string username = doc.DocumentNode.SelectSingleNode("//div[@class='infotext']").InnerText;
+                txtOutput.Text = username;
+                html = rq.Get("https://id.zing.vn/v2/infosetting/personal").ToString();
+                doc.LoadHtml(html);
+                string personal_tokenCaptcha = doc.DocumentNode.SelectSingleNode("//input[@id='personal_tokenCaptcha']").Attributes["value"].Value;
+                Random rn = new Random();
+                string jsrandom = rn.Next(400000, 600000).ToString("D6");
+                string demohtml = rq.Get($"https://id.zing.vn/ajax/gentoken?callback=zmCore.js{jsrandom}").ToString();
+                string tokenExpire = Regex.Match(demohtml, "msg\":\"([^\"]+)").Groups[1].Value;
+
+                rq.AddUrlParam("action", "personal.update");
+                rq.AddUrlParam("fullname", "Moi update 2 ");
+                rq.AddUrlParam("gender", "0");
+                rq.AddUrlParam("add", "dia chi ao 2");
+                rq.AddUrlParam("dob", "3");
+                rq.AddUrlParam("mob", "4");
+                rq.AddUrlParam("yob", "2000");
+                rq.AddUrlParam("city", "53");
+                rq.AddUrlParam("occupation", "4");
+                rq.AddUrlParam("maritalstatus", "0");
+                rq.AddUrlParam("tokenCaptcha", personal_tokenCaptcha);
+                rq.AddUrlParam("tokenExpire", tokenExpire);
+                rq.AddUrlParam("callback", $"zmCore.js{jsrandom}");
+                html = rq.Get("https://id.zing.vn/v2/infosetting/personal/update").ToString();
+                if (html.Contains("Cập nhật thành công"))
+                    MessageBox.Show("Update Success!");
+                else
+                    MessageBox.Show("Error!");
+                Dictionary<int, string> city = new Dictionary<int, string>();
+                city.Add(31, "An Giang");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Console.WriteLine(ex.ToString());
+            }
+        
+        }
+    }
+}
